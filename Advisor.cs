e1157e0c740391e3b595fa5e5753382d9dc99085
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-07-21 15:45:22 -0700

    added student and advisor, branched from master to main

---
+++ Advisor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    internal class Advisor
+    {
+        public string FullName { get; set; }
+        /// <summary>
+        /// Employees full legal name
+        /// </summary>
+        public string SchoolEmail { get; set; }
+        /// <summary>
+        /// employees assigned work email
+        /// </summary>
+        public string OfficeLocation { get; set; }
+        /// <summary>
+        /// Where the employees personal office is located
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// The employees work phone number
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
+        /// <summary>
+        /// The employees Assigned Advisor
+        /// </summary>
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        /// <summary>
+        ///legal first name of a college student of a college
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        ///legal last name of a college student of a college
+        /// </summary>
+        public string SchoolEmail { get; set; }
+        /// <summary>
+        ///assigned email of a college student of a college
+        /// </summary>
+        
+        public string PhoneNumber { get; set; }
+
+    }
+
+}
